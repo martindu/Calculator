ef934ff75d9fedf9882337541c9f282f8d8ed2e7
martindu <martindu@umich.edu>
2015-03-06 16:52:07 -0800

    Result text has to be changed

--- app/src/main/java/com/example/adurrel/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/adurrel/mycalculator/MainActivity.java
@@ -4,15 +4,95 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText operand1;
+    EditText operand2;
+    EditText result;
+    Button btnPlus;
+    Button btnMultiply;
+    Button btnDivide;
+    Button btnClear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //operands from main activity
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        result = (EditText) findViewById(R.id.editResult);
+
+        //buttons from activity_main.xml
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnClear = (Button) findViewById(R.id.btnClear);
+
+        //Add functionality
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float oper1 = Float.parseFloat(operand1.getText().toString());
+                float oper2 = Float.parseFloat(operand2.getText().toString());
+                result.setText(String.valueOf((oper1 + oper2)));
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float oper1 = Float.parseFloat(operand1.getText().toString());
+                float oper2 = Float.parseFloat(operand2.getText().toString());
+                result.setText(String.valueOf((oper1 * oper2)));
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float oper1 = Float.parseFloat(operand1.getText().toString());
+                float oper2 = Float.parseFloat(operand2.getText().toString());
+
+                if(oper2 == 0){
+                    Toast toast = Toast.makeText(getApplicationContext(),"You can not divide no by zero",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                else {
+                    result.setText(String.valueOf((oper1 / oper2)));
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.setText("");
+                operand2.setText("");
+                result.setText("0.0");
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
     }
+
 
 
     @Override
